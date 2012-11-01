b76fcfcdf1d69b3002f7f4420b70e0ab9a9c6765
AlexanderPavlenko <a.pavlenko@roundlake.ru>
2012-11-01 21:15:09 +0400

    and shallow_path may be blank

--- lib/rails/resources_with_joosy.rb
+++ lib/rails/resources_with_joosy.rb
@@ -4,7 +4,7 @@ module ActionDispatch::Routing::Mapper::Resources
   def resources_with_joosy(*resources, &block)
     resources_without_joosy(*resources, &block).tap do
       namespace = Joosy::Rails::Engine.resources[@scope[:module].to_s] ||= {}
-      namespace[resources[0].to_s.singularize] = "#{@scope[:shallow_path]}/#{resources[0]}"
+      namespace[resources[0].to_s.singularize] = "#{@scope[:path]}/#{resources[0]}"
     end
   end
   alias_method_chain :resources, :joosy

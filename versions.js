971ad2398c30ba769e0b79305310d6069baf9c06
Boris Staal <boris@staal.io>
2013-09-03 04:00:06 +0700

    Updates

--- versions.js
+++ versions.js
@@ -1,1 +1,1 @@
-window.versions = {"edge":{"version":"1.2.0-alpha","date":"2 September 2013"},"stable":{"version":"1.1.2","date":"11 June 2013"},"versions":[{"version":"1.0.0","date":"1 January 2013"}]}
+window.versions = {"edge":{"version":"1.2.0-alpha","date":"3 September 2013"},"stable":{"version":"1.1.2","date":"11 June 2013"},"versions":[{"version":"1.0.0","date":"1 January 2013"}]}
